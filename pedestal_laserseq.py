3c09895dca2f64f505a077563eb43746ccdbd9a7
Stefano Argiro <Stefano.Argiro@cern.ch>
2016-11-11 12:23:23 +0100

    added B field

--- pedestal_laserseq.py
+++ pedestal_laserseq.py
@@ -7,43 +7,46 @@ from ElectronicsMapping import ElectronicsMapping
 from ROOT import TTree,TFile
 from array  import array
 
-dstdir='/cmsecallaser/srv-ecal-laser-13/disk0/ecallaser/dst.merged.matacq1.2.2016'
+#dstdir='/cmsecallaser/srv-ecal-laser-13/disk0/ecallaser/dst.merged.matacq1.2.2016'
+dstdir='/cmsecallaser/srv-ecal-laser-13/disk0/ecallaser/dst.merged.matacq1.2'
 
 
-
-def readpedestals(fed,runmin=273186,runmax=279728):
+def readpedestals(fed,runmin=273186,runmax=283408):
     """ make a TTree with all channels in the given run range"""
  
 
-    nchs = 75848
-    nebchs= 61200
+    nchs= 1700
+
 
     color = 447
 
     timestamp = array('i',[0]) 
     run     = array('i',[0])
+    fedid   = array('i',[0])
     ped     = array('f',[0.0]*nchs)
     pedrms  = array('f',[0.0]*nchs)
     tpped   = array('f',[0.0]*nchs)
     tppedrms= array('f',[0.0]*nchs)
     lped    = array('f',[0.0]*nchs)
     lpedrms = array('f',[0.0]*nchs)
+    bfield  = array('f',[0])
 
     rfile= TFile('pedestals_fed'+str(fed)+'_'+str(runmin)+'_'+str(runmax)+'.root','recreate')
     tree = TTree('peds','pedestals')
     
     tree.Branch('timestamp',timestamp,'timestamp/I')
     tree.Branch('run',run,'run/I')
+    tree.Branch('fed',fedid,'fed/I')
     tree.Branch('ped',ped,'ped['+str(nchs)+']/F')
     tree.Branch('pedrms',pedrms,'pedrms['+str(nchs)+']/F')
     tree.Branch('tpped',tpped,'tpped['+str(nchs)+']/F')
     tree.Branch('tppedrms',tppedrms,'tppedrms['+str(nchs)+']/F')
     tree.Branch('lped',lped,'lped['+str(nchs)+']/F')
     tree.Branch('lpedrms',lpedrms,'lpedrms['+str(nchs)+']/F')
-    
+    tree.Branch('bfield',bfield,'bfield/F')
 
-    
-    #loop on feds        
+    fedid[0] = fed
+       
     allfiles = glob.glob(dstdir+'/'+str(fed)+'/'+'*'+str(color)+'*')
     filelist=[]
 
@@ -71,23 +74,14 @@ def readpedestals(fed,runmin=273186,runmax=279728):
         header = lines[0]
         run[0]       = int(header.split()[0])
         timestamp[0] = int(header.split()[3])
-
+        bfield[0]    = float(header.split()[10])
 
 
         for line in lines[2:]:
 
             if len(line) <10 : break
             channel   = int(line.split()[1])   
-            detid     = em.detidFromElecmapping(fed,channel)  
-            subdet    = int((detid>>25)&0x7) # 1 EB, 2 EE
-        
-
-            if subdet == 1 :
-                ebdetid = EBDetId(detid)
-                idx = ebdetid.hashedIndex()
-            else :
-                eedetid = EEDetId(detid)
-                idx = eedetid.hashedIndex() + nebchs 
+            idx = channel
  
             ped[idx]        = float(line.split()[13])
             pedrms[idx]     = float(line.split()[14]) 
@@ -100,21 +94,19 @@ def readpedestals(fed,runmin=273186,runmax=279728):
 
     tree.Write()
 
+
 # main
-
-
-
-#readpedestals(runmin=273186,runmax=279728) 
-#readpedestals(runmin=273186,runmax=274200)
 
 if __name__ == '__main__':
 
     _parallelProcessing_ = False
-    runmin=273186
-    runmax=279728
-    
-    #ecalfeds = range(601,655)
-    ecalfeds = range(649,655)
+    #runmin=280255
+    #runmax=280396
+    runmin = 273186
+    runmax = 283408
+
+    ecalfeds = range(601,655)
+
 
     if _parallelProcessing_: # careful, could overload the laser farm
         jobs = []
